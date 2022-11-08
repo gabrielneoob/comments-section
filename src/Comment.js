8c5950d1a83b550ec9986493551911ad65379267
Neoob <gabrielngc@gmail.com>
2022-11-08 18:44:11 -0300

    add user-information

--- src/Comment.js
+++ src/Comment.js
@@ -1,15 +1,41 @@
 import React from 'react'
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, dados }) => {
+
+  function teste() {
+    const newDados = dados.comments = [...dados.comments, { id: 3, content: 'oi' }]
+    console.log(dados);
+  }
+
+
   return (
     <div className='comment-box'>
       <div className='score-box'>
-        <div className='plus'><i class="fa-solid fa-plus"></i></div>
-        <div className=''>{comment.score}</div>
-        <div className='minus'><i class="fa-solid fa-minus"></i></div>
+        <div className='plus'><i className="fa-solid fa-plus"></i></div>
+        <div className='score' onClick={teste}>{comment.score}</div>
+        <div className='minus'><i className="fa-solid fa-minus"></i></div>
       </div>
 
-    </div>
+      <div className='user-container'>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div className='user-information'>
+            <div className='user-img'>
+              <img src={require(`./assets/images/avatars/image-${comment.user.username}.png`)}></img>
+            </div>
+            <h3 className='user-name'>{comment.user.username}</h3>
+            <div className='created-at'>{comment.createdAt}</div>
+          </div>
+
+          <div className='reply'>
+            <i class="fa-sharp fa-solid fa-reply"></i>
+            <span>Reply</span>
+          </div>
+        </div>
+
+
+      </div>
+
+    </div >
   )
 }
 
