8ed8018b0b7989f73bc590e10043e8a90692877c
Neoob <gabrielngc@gmail.com>
2022-11-08 13:40:13 -0300

    set dados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import dataJson from './data.json';
 
 function App() {
+  const [dados, setDados] = useState(dataJson);
+
+
+
+  useEffect(() => {
+    console.log()
+
+  }, [])
+
   return (
     <div className="app">
-      <h1>oi</h1>
     </div>
   );
 }
